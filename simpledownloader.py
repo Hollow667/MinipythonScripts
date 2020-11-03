322e7c6f6ff96d148419663982b5846116126ad5
StarkGang <StarkGangZ@gmail.com>
2020-11-03 14:50:57 +0530

    last fix

--- simpledownloader.py
+++ simpledownloader.py
@@ -1,13 +1,16 @@
+
 # First, install wget by 'pip install wget'
 import os
-import wget
-
+try:
+    import wget
+except:
+    os.system('pip install wget')
 starkislub = input("Give Download Link. \n")
-pathdebc = input(r"Give Path Where This File should be Saved. \n")
-print("Ok, Let me Download this.")
+pathdebc = input(r"Give Path Where This File should be Saved." + " \n")
+print("Ok, Let me try to Download this.")
 try:
   sedlyf = wget.download(starkislub, out = pathdebc)
-  print("Downloaded Sucessfully in {sedlyf}.")
+  print(f"Downloaded Sucessfully in {sedlyf}.")
   print("---------- + ----------")
 except Exception as e:
-  print("I can't download it \nReason : {e}")
+  print(f"I can't download it \nReason : {e}")
